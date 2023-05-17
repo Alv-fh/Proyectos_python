ef1939ae7b3c2c3c34cf4664e5d5126ebf6e826f
Alvaro Falcon <alvaritofalcon26@gmail.com>
2023-05-17 23:13:00 +0200

    Añadí Prueba1.py

---
+++ Prueba1.py
@@ -0,0 +1,14 @@
+# Variable
+
+var1 = input ("Nombre->> ")
+ape = input ("Apellido->> ")
+
+
+if var1 == "Alvaro":
+    if ape == "Falcon":
+        print ("Acceso concedido")
+    else:
+        print ("Acceso denegaddo")
+else:
+    print ("Acceso denegado")
+
